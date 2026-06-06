18bbdd193620907ef67289457c95a844ff162c5e
MgKhai <195129227+MgKhai@users.noreply.github.com>
2026-06-06 10:16:27 +0630

    feat:auth_check_added

--- src/routes/v1/auth/auth.ts
+++ src/routes/v1/auth/auth.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { RequestHandler } from "express";
 import {
   confirmPassword,
   login,
@@ -8,8 +8,11 @@ import {
   forgetPassword,
   verifyForgetOtp,
   resetPassword,
+  authCheck,
 } from "../../../controllers/authController";
 import { Request, Response, NextFunction } from "express";
+import { auth } from "../../../middleware/auth";
+
 const router = express.Router();
 
 router.post("/register", register);
@@ -21,6 +24,7 @@ router.post("/logout", logout);
 router.post("/forget-password", forgetPassword);
 router.post("/verify-forget-otp", verifyForgetOtp);
 router.post("/reset-password", resetPassword);
+router.get("/auth-check", auth, authCheck as RequestHandler);
 
 router.get("/testing", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({ message: "it is okay" });
